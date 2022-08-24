7a96e5fe134a71fdcb07f043bce4e67ca7ad2ecb
hilalhaniefam <49035980+hilalhaniefam@users.noreply.github.com>
2022-08-25 06:00:41 +0700

    add search in userdata

--- lib/api_services/api_services.dart
+++ lib/api_services/api_services.dart
@@ -8,17 +8,30 @@ class GetUser {
   var data = [];
   List<User> results = [];
 
-  Future<List<User>> getAllUsers() async {
+  Future<List<User>> getAllUsers({String? query}) async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     var userData =
         jsonDecode(sharedPreferences.getString('userData').toString());
     String token = userData['accessToken'];
     var response = await http.get(Uri.parse('$ipurl/users'),
         headers: {'Authorization': 'Bearer $token'});
-    Map<String, dynamic> responseBody = jsonDecode(response.body);
-    List<dynamic> data = responseBody['payload'];
-    print(data);
-    results = data.map((data) => User.fromJson(data)).toList();
+    try {
+      if (response.statusCode == 200) {
+        Map<String, dynamic> responseBody = jsonDecode(response.body);
+        List<dynamic> data = responseBody['payload'];
+        print(data);
+        results = data.map((data) => User.fromJson(data)).toList();
+        if (query != null) {
+          results = results
+              .where((u) => u.name!.toLowerCase().contains(query.toLowerCase()))
+              .toList();
+        }
+      } else {
+        print('API error');
+      }
+    } on Exception catch (e) {
+      print(e);
+    }
     return results;
   }
 }
--- lib/pages/admin/userdata_page.dart
+++ lib/pages/admin/userdata_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:snippet_coder_utils/hex_color.dart';
 import 'package:talanoa_app/api_services/api_services.dart';
 import 'package:talanoa_app/api_services/user_model.dart';
-import 'package:talanoa_app/pages/admin/search.dart';
+import 'package:talanoa_app/widgets/admin/search_user.dart';
 import 'package:talanoa_app/widgets/shared/app_bar.dart';
 
 class Userdata extends StatefulWidget {
@@ -23,8 +23,7 @@ class _UserdataState extends State<Userdata> {
             backButton: _handleBack,
             title: 'User Data',
             onPressed: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (_) => const SearchPage()));
+              showSearch(context: context, delegate: SearchUser());
             }),
         backgroundColor: HexColor('A7B79F'),
         body: FutureBuilder<List<User>>(
@@ -36,67 +35,72 @@ class _UserdataState extends State<Userdata> {
                 child: CircularProgressIndicator(),
               );
             }
-            return ListView.builder(
-                itemCount: data.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      margin:
-                          const EdgeInsets.only(left: 36, top: 29, right: 78),
-                      color: HexColor('F1ECE1'),
-                      child: SizedBox(
-                        width: 261,
-                        height: 111,
-                        child: Column(
-                          children: [
-                            Align(
-                              alignment: Alignment.topLeft,
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.only(left: 34, top: 22),
-                                child: Text(
-                                  data[index].name,
-                                  style: const TextStyle(
-                                      fontFamily: 'Josefin Sans',
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                              ),
-                            ),
-                            Align(
-                              alignment: Alignment.topLeft,
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.only(left: 34, top: 6),
-                                child: Text(
-                                  data[index].phone,
-                                  style: const TextStyle(
-                                      fontFamily: 'Josefin Sans',
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                              ),
-                            ),
-                            Align(
-                              alignment: Alignment.topLeft,
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.only(left: 34, top: 6),
-                                child: Text(
-                                  data[index].email,
-                                  style: const TextStyle(
-                                      fontFamily: 'Josefin Sans',
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                              ),
-                            ),
-                          ],
+            return RefreshIndicator(
+              onRefresh: () async {
+                _userList.getAllUsers();
+              },
+              child: ListView.builder(
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
                         ),
-                      ));
-                });
+                        margin:
+                            const EdgeInsets.only(left: 36, top: 29, right: 78),
+                        color: HexColor('F1ECE1'),
+                        child: SizedBox(
+                          width: 261,
+                          height: 111,
+                          child: Column(
+                            children: [
+                              Align(
+                                alignment: Alignment.topLeft,
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.only(left: 34, top: 22),
+                                  child: Text(
+                                    data[index].name,
+                                    style: const TextStyle(
+                                        fontFamily: 'Josefin Sans',
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.topLeft,
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.only(left: 34, top: 6),
+                                  child: Text(
+                                    data[index].phone,
+                                    style: const TextStyle(
+                                        fontFamily: 'Josefin Sans',
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.topLeft,
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.only(left: 34, top: 6),
+                                  child: Text(
+                                    data[index].email,
+                                    style: const TextStyle(
+                                        fontFamily: 'Josefin Sans',
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ));
+                  }),
+            );
           },
         ));
   }
